fe5f7aa2625a9adb47221635e671e22c1d8c9404
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-03-25 08:49:19 +0800

    export multiple function

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import add from '../Utilities/Calculates';
-// import { add, multiply } from '../Utilities/Calculates';
+// import add from '../Utilities/Calculates';
+import { add, multiply } from '../Utilities/Calculates';
 
 
 const Cosmetics = () => {
     const first= 55;
     const second= 66;
     const totalAddition= add(first, second)
-   
+    const totalMultiply= multiply(first, second)
     return (
         <div>
             <h1>welcome to my Cosmetics components!!!</h1>
             <h3>Total-addition= {totalAddition}</h3>
-            
+            <h3>Total-multiply= {totalMultiply}</h3>
         </div>
     );
 };
--- src/components/Utilities/Calculates.js
+++ src/components/Utilities/Calculates.js
@@ -2,5 +2,11 @@ const add = (first, second) =>{
    return first + second;
 }
 // export for one things
-export default add;
+// export default add;
 
+const multiply = (first, second) =>{
+return first*second;
+}
+
+// to export one more things
+export {add, multiply}
